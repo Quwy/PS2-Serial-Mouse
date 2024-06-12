d9cb3651aeed0850f9641cf470f409f1b4dd816e
Quwy <38657498+Quwy@users.noreply.github.com>
2024-06-12 20:39:23 +0300

    Update Defines.h

--- Firmware/Defines.h
+++ Firmware/Defines.h
@@ -14,10 +14,21 @@
 #define VERSION "1.00b" // not used in the code, just text tag
 
 /* UART SECTION */
-//#define UART_9600_BPS // define for 9600 bps, otherwise 1200 bps wiil used. note what 9600 bps is not common and will not work with most mouse drivers.
+//#define UART_9600_BPS // define for 9600 bps, otherwise 1200 bps wiil used. note what 9600 bps is not common and will not work with most mouse drivers
 #define BUTTONS     3 // assume 3-button mouse (no risk if 2-button mouse attached)
 //#define RESET_AS_DTR // define if pin 1 (RESET/PB5) used as GPIO for DTR line input
 
+/* FUSES SECTION */
+// these values not used in the code, but will useful to configure microcontroller
+#ifdef RESET_AS_DTR
+    #define efuse 0x01
+    #define lfuse 0xc1
+    #define hfuse 0x7f
+#else
+    #define efuse 0x01
+    #define lfuse 0xc1
+    #define hfuse 0xdf
+#endif
 
 /* DEBUG SECTION */
 //#define DUMMY_TEST_MODE // just repeat all incoming data from PS/2 to UART (high bit truncated)
